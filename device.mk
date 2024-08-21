4d2d2788ca2f9cf56be9459dba6ef5808bf0c680
romgharti <romgharti@gmail.com>
2024-08-21 19:59:47 +0800

    LG8n: Build android.hardware.audio.sounddose-vendor-impl
    E audiohalservice: Failed to dlopen android.hardware.audio.sounddose-vendor-impl.so: dlopen failed: library android.hardware.audio.sounddose-vendor-impl.so not found
    W audiohalservice: createISoundDoseFactory() from android.hardware.audio.sounddose-vendor-impl failed
    Signed-off-by: romgharti <romgharti@gmail.com>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -52,6 +52,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio@7.0-impl \
+    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
